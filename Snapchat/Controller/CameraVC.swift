1a36aef3e1a6314fc0435c73235631432d323906
Araz Sinjary <araz@Arazs-MBP.Home>
2018-09-08 21:37:33 +0300

    stater project

---
+++ Snapchat/Controller/CameraVC.swift
@@ -0,0 +1,23 @@
+//
+//  CameraVC.swift
+//  Snapchat
+//
+//  Created by Araz Sinjary on 9/8/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: AVCamCameraViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+   
+
+
+}
+
